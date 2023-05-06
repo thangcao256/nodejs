0a5ffe0ee6d4903e8dbb5ea5b93ac0926ea50589
Cao Minh Thang <90888667+thangcao256@users.noreply.github.com>
2023-05-06 14:57:50 +0700

    Hello World!
    Hello World with ExpressJS

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-    console.log('run request ...')
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<h3>Hello world! </h3>');
-    res.write('<h2>From Thang Cao with love </h2>');
-    res.end();
+// const server = http.createServer((req, res) => {
+//     console.log('run request ...')
+//     res.setHeader('Content-Type', 'text/html');
+//     res.write('<h3>Hello world! </h3>');
+//     res.write('<h2>From Thang Cao with love </h2>');
+//     res.end();
+// })
+
+// server.listen(3000, 'localhost', () => {
+//     console.log('Node.JS server is running on port: 3000');
+// })
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
 })
 
-server.listen(3000, 'localhost', () => {
-    console.log('Node.JS server is running on port: 3000');
+app.get('/cmt', (req, res) => {
+    res.send('Hello Cao Minh Thang!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
 })
