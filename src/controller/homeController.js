b1160bd4cd3172b9a36aad93ecdf538dd6c7fae7
Cao Minh Thang <90888667+thangcao256@users.noreply.github.com>
2023-05-07 15:53:26 +0700

    HTTP REQUEST
    http request and database

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,7 +1,25 @@
+import connection from "../configs/connectDB"
+
 let getHomepage = (req, res) => {
 
     // logic
-    return res.render('test/index.ejs')
+    let data = [];
+    connection.query(
+        'SELECT * FROM `users` ',
+        function (err, results, fields) {
+            // console.log(results); // results contains rows returned by server
+            // console.log(fields); // fields contains extra meta data about results, if available
+            results.map((row) => {
+                data.push({
+                    id: row.id,
+                    firstName: row.firstName,
+                    lastName: row.lastName,
+                    email: row.email,
+                    address: row.address
+                })
+            });
+            return res.render('test/index.ejs', { dataUser: JSON.stringify(data) });
+        });
 }
 
 module.exports = {
