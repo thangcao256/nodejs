53eef893b70224e65c661b821d58532e78593ff7
Cao Minh Thang <90888667+thangcao256@users.noreply.github.com>
2023-05-08 13:15:16 +0700

    CRUD
    read update delete user

--- src/route/web.js
+++ src/route/web.js
@@ -5,13 +5,16 @@ let router = express.Router();
 const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
 
-    router.get('/about', (req, res) => {
-        res.send(`I'm Minh Thang!`)
-    })
-
     router.get('/details/user/:userId', homeController.getDetailPage)
 
     router.post('/create-new-user', homeController.createNewUser)
+
+    router.post('/delete-user', homeController.deleteUser);
+
+    router.get('/edit-user/:id', homeController.getEditPage);
+
+    router.post('/update-user', homeController.postUpdateUser)
+
     // return app.use('/web/v1/', router)
     return app.use('/', router)
 
