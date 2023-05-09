a16f8f24ccf72f4f16c1e96e54a7ad09192ef0ce
Cao Minh Thang <90888667+thangcao256@users.noreply.github.com>
2023-05-09 13:59:34 +0700

    MIDDLEWARE

--- src/server.js
+++ src/server.js
@@ -5,9 +5,19 @@ import initAPIRoute from './route/api';
 import connection from './configs/connectDB';
 
 require('dotenv').config();
+var morgan = require('morgan')
 
 const app = express();
 const port = process.env.PORT || 8081;
+
+app.use((req, res, next) => {
+    //check => return res.send()
+    console.log('>>> run into my middleware')
+    console.log(req.method)
+    next();
+})
+
+app.use(morgan('combined'))
 
 // support send data from client to server
 app.use(express.urlencoded({ extended: true }));
@@ -21,6 +31,10 @@ initWebRoute(app);
 
 // init api route
 initAPIRoute(app);
+
+app.use((req, res) => {
+    return res.render('404.ejs')
+})
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
